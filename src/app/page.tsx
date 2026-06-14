2f387cb3ddaef6c6218cead30e0cb44b2c712367
Vladyslav <infuzoriya52@gmail.com>
2026-06-15 01:33:23 +0200

    test: verify CI failure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const value: string = 123;
+  const value: number = 123;
   return (
     <div className={styles.page}>
       <main className={styles.main}>
