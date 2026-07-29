8b1112f212066f2dbad4b655391915bb2b291f8f
Vladyslav <78151203+kanderyabl@users.noreply.github.com>
2026-07-30 01:06:21 +0200

    . (#24)

--- src/features/auth/change-phone/model/changePhoneSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-import { createPhoneSchema, type PhoneValidationMessages } from "../../model";
-
-export function createChangePhoneSchema(messages: PhoneValidationMessages) {
-  return z.object({
-    phone: createPhoneSchema(messages),
-  });
-}
--- src/features/auth/change-phone/ui/index.ts
+++
@@ -1,2 +0,0 @@
-export { ChangePhoneForm } from "./ChangePhoneForm";
-export { ChangePhoneModal } from "./ChangePhoneModal";
